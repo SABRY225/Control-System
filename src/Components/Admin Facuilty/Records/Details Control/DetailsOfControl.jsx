0abc9ff3a843de25eaf8d64b2c7e9bb8cc388cf9
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-04-30 17:24:40 +0300

    add files

--- src/Components/Admin Facuilty/Records/Details Control/DetailsOfControl.jsx
+++ src/Components/Admin Facuilty/Records/Details Control/DetailsOfControl.jsx
@@ -8,9 +8,11 @@ export default function DetailsOfControl() {
             <div className='DetailsOfControl-Title'>بيانات الكنترول</div>
             <div className='DetailsOfControl-input'><input type="text" disabled value="اسم الكنترول" className='DetailsOfControl-nameofcontrol'/></div>
             <div className='DetailsOfControl-input'>
-                <input type="text" className='DetailsOfControl-Yeay' disabled value="الفصل الدراسي" />
+                <input type="text" className='DetailsOfControl-Yeay' disabled value="المستوي او الشعبة"/>
+                <input type="text" className='DetailsOfControl-Yeay DetailsOfControl-Yeay-2' disabled value="الفصل الدراسي" />
                 <input type="text" className='DetailsOfControl-Yeay DetailsOfControl-Yeay-2'disabled value="العام الأكاديمي" />
             </div>
+            
             <div className='DateOfControl' >
                 <div className='column'>
                     <div className='TextDate'>اليوم الأخير</div>
@@ -20,8 +22,9 @@ export default function DetailsOfControl() {
                     <div className='TextDate'>اليوم الأول</div>
                     <input type="text" value="dd/mm/yyyy" className='DetailsOfControl-Date' disabled />
                 </div>
+                
                 <div className='column'>
-                    <div className='dateofcontrolText'>مواعيد الكنترول</div>
+                    <div className='dateofcontrolText'>مواعيد العمل في الكنترول</div>
                 </div>
             </div>
             <hr />
--- src/Components/Admin Facuilty/Records/Details Control/Tasks.jsx
+++ src/Components/Admin Facuilty/Records/Details Control/Tasks.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 export default function Tasks() {
   return (
-    <div>Tasks</div>
+    <div className='TaskOfControl'>
+    <div className='TaskOfControl-Title'>المهام</div>
+    <div className='container Task-Groupes' >
+        <div className='row'>
+          {/* Start */}
+        <div class="col-6 box m-3">
+            <div className='box_Title'>حصد دراجات ماده 1 </div>
+            <div className="row">
+              <div className="col dataofTask">
+                <div className="dataOfTask">3/24/2003</div>
+              </div>
+              <div className="col memberOfTask">
+                <div className='nameOfMember'>د/ محمد عبدالرازق</div>
+                <div className='nameOfMember'>د/ محمد عبدالرازق</div>
+              </div>
+            </div>
+        </div>
+        <div class="col-6 box  m-3">
+        <div className='box_Title'>حصد دراجات ماده 1 </div>
+            <div className="row">
+              <div className="col dataofTask">
+                <div className="dataOfTask">3/24/2003</div>
+              </div>
+              <div className="col memberOfTask">
+                <div className='nameOfMember'>د/ محمد عبدالرازق</div>
+                <div className='nameOfMember'>د/ محمد عبدالرازق</div>
+              </div>
+            </div>
+        </div>
+        {/* End */}
+        </div>
+    </div>
+    <hr />
+    </div>
   )
 }
 
