f861577ad617f79d923c50bee148eefaedaab159
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2024-05-06 23:15:18 +0300

    all done

--- src/Components/Admin Facuilty/Register/Members.jsx
+++ src/Components/Admin Facuilty/Register/Members.jsx
@@ -1,10 +1,46 @@
-import React, { useState } from 'react'
+import "./FromRegisterStyle.css";
+import React, { useState } from "react";
 
 export default function Members() {
-  
   return (
     <>
-     Members
+      <div className="containerRegister">
+        <form action="#" style={{display: "flex",flexDirection: "column"}}>
+          <div className="row">
+            <div className="control-row-register col">
+              <label htmlFor="name">Name</label>
+              <input id="name" type="text" name="name" />
+            </div>
+            <div className="control-row-register col">
+              <label htmlFor="username">name-user</label>
+              <input id="username" type="text" name="userName" />
+            </div>
+            <div className="control-row-register col">
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" name="email" />
+            </div>
+            <div className="control-row-register col">
+              <label htmlFor="password">Password</label>
+              <input id="password" type="text" password="name" />
+            </div>
+            <div className="control-row-register col">
+              <label htmlFor="cp">Confirm Password</label>
+              <input id="cp" type="password" name="password" />
+            </div>
+            <div className="control-row-register col">
+              <label htmlFor="scientificDegree">Scientific Degree</label>
+              <input
+                id="scientificDegree"
+                type="text"
+                name="scientificDegree"
+              />
+            </div>
+          </div>
+          <button type="submit" class="mt-3">
+            اضافة عضو جديد
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
