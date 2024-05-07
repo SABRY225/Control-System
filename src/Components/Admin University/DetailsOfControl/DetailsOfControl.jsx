7b78f588df713eef85346f4f832eea1f0ff2b0cf
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2024-05-07 20:22:30 +0300

    *

--- src/Components/Admin University/DetailsOfControl/DetailsOfControl.jsx
+++ src/Components/Admin University/DetailsOfControl/DetailsOfControl.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setDetails } from '../../../Redux/detailsSlice';
-import { academicYears } from './academicYears';
-import './DetailsOfControl.css';
-import { useNavigate } from 'react-router-dom'; 
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setDetails } from "../../../Redux/detailsSlice";
+import { academicYears } from "./academicYears";
+import "./DetailsOfControl.css";
+import { useNavigate } from "react-router-dom";
 
-const DetailsOfControl = ({ controls }) => {
-  // const semester = controls["acaD_YEAR"]; // Mocked semester value (can be fetched from admin settings or API)
+const DetailsOfControl = ({ faculty }) => {
+  const { controls } = faculty;
+
+  const semester = `${new Date().getFullYear()}/${
+    new Date().getFullYear() + 1
+  }`;
+  // Mocked semester value (can be fetched from admin settings or API)
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleYearClick = (yearInfo) => {
-    const { year, committeeName, committeeNickname, status } = yearInfo;
-    // dispatch(setDetails({ year, committeeName, committeeNickname, status, , semester}));
-    console.log("clicked !!");
+    dispatch(setDetails({ control:yearInfo, faculty }));
+    // console.log("clicked !!");
     // console.log(year, committeeName, committeeNickname, status, faculty, semester);
-    navigate('./AcademicYearDetails');
+    navigate("./AcademicYearDetails");
   };
-
+  // console.log(controls);
   return (
     <div className="container mt-4">
       <div className="page">
         <div className="row justify-content-center text-center years-of-controls">
-          {/* <div className="col-md-12 my-3">
+          <div className="col-md-12 my-3">
             <h4>Semester - {semester}</h4>
-          </div> */}
+          </div>
 
           {/* Displaying Academic Years */}
           {controls.map((control) => (
@@ -34,12 +38,12 @@ const DetailsOfControl = ({ controls }) => {
                 className="d-flex flex-column justify-content-center align-items-center rounded-2 mt-4 p-3 border border-3"
                 onClick={() => handleYearClick(control)} // Call handleYearClick on click
               >
-                <div className="d-flex">
-                  <h5>{control.acaD_YEAR}</h5>
-                  <p className="mx-5">
-                    {control.committeeName} <br />
+                <div className="d-flex w-100 justify-content-evenly">
+                  <h5>{control.name}</h5>
+                  <p>
+                    {control.userCreator.name} <br />
                     <span className="fw-bold" style={{ color: "#43BBFF" }}>
-                      {control.committeeNickname}
+                      رائس الكلية
                     </span>
                   </p>
                 </div>
--- src/Redux/detailsSlice.jsx
+++ src/Redux/detailsSlice.jsx
@@ -1,26 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  year: '',
-  committeeName: '',
-  committeeNickname: '',
-  status: '',
-  faculty: '',
-  semester: '',
+  faculty: "",
+  control: "",
 };
 
 const detailsSlice = createSlice({
-  name: 'details',
+  name: "details",
   initialState,
   reducers: {
     setDetails: (state, action) => {
-      const { year, committeeName, committeeNickname, status, faculty, semester } = action.payload;
-      state.year = year;
-      state.committeeName = committeeName;
-      state.committeeNickname = committeeNickname;
-      state.status = status;
+      const { control, faculty } = action.payload;
+      state.control = control;
       state.faculty = faculty;
-      state.semester = semester;
     },
   },
 });
