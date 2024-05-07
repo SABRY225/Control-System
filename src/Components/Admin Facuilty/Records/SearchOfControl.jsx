15c8493ced39841d4518c5f7e44c6164d2162d4a
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-05-07 20:56:27 +0300

    add data

--- src/Components/Admin Facuilty/Records/SearchOfControl.jsx
+++ src/Components/Admin Facuilty/Records/SearchOfControl.jsx
@@ -13,19 +13,16 @@ export default function SearchOfControl() {
                 </div>
                 <div className='SearchBar row text-center '>
                             <div className="col-md-6">
-                            <select name="Semester_Year" id="Input_Select">
-                                <option defulat>الفصل الدراسي</option>
-                                <option value="الاول">الاول</option>
-                                <option value="الثاني">الثاني</option>
-                                <option value="الفصل الصيفي">الفصل الصيفي</option>
-                            </select>                                
-                            </div>
-                            <div className="col-md-6">
                             <select name="Acad_Year" id="Input_Select">
                                 <option defulat>العام الأكاديمي</option>
-                                <option value="2023-2024">2023-2024</option>
-                                <option value="2024-2025">2024-2025</option>
-                                <option value="2025-2026">2025-2026</option>
+                                <option value="2023/2024">2023/2024</option>
+                                <option value="2024/2025">2024/2025</option>
+                                <option value="2025/2026">2025/2026</option>
+                                <option value="2026/2027">2026/2027</option>
+                                <option value="2027/2028">2027/2028</option>
+                                <option value="2028/2029">2028/2029</option>
+                                <option value="2029/2030">2029/2030</option>
+                                <option value="2031/2031">2031/2031</option>
                             </select>                                
                             </div>
                 </div>
