949969a9374007b048563784eb8da96ffa0bf371
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2024-05-08 03:48:58 +0300

    *

--- src/Components/Admin Facuilty/Controls/AnalysisControl.jsx
+++ src/Components/Admin Facuilty/Controls/AnalysisControl.jsx
@@ -1,181 +1,187 @@
-import React from 'react'
-import accepted from "../../../assets/accepted.png"
-import notaccepted from "../../../assets/notAccepted.png"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'react-chartjs-2'
-ChartJS.register(
-    ArcElement,
-    Tooltip,
-    Legend
-);
+import React, { useCallback, useEffect, useState } from "react";
+import accepted from "../../../assets/accepted.png";
+import notaccepted from "../../../assets/notAccepted.png";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 export default function AnalysisControl() {
-    const data = {
-        labels: ['المواد التي تم انجازها', 'المواد التي لو يتم انجازها'],
-        datasets: [{
-            lable: 'Poll',
-            data: [60, 40],
-            backgroundColor: ['rgba(68, 170, 68, 1)', 'red'],
-            borderColor: ['black', 'red'],
-        }]
-    }
-    const options = {
+  const control = useSelector((state) => state.details.control);
+  const tok = useSelector((state) => state.auth.token);
+  const HeadControl = control.user.name;
+  const [controlSubjects, setControlsSubject] = useState([]);
+  const [Notes, setNotes] = useState([]);
+  const getControlSubject = useCallback(() => {
+    const getControlSubject = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:5120/Subject/subjects-of-control?controld=" +
+            control.control.id,
+          {
+            headers: {
+              Authorization: "Bearer " + tok,
+            },
+          }
+        );
+        setControlsSubject(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getControlSubject();
+  }, []);
+    
+  const getNote = useCallback(() => {
+    const getNote = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:5120/controlnotes/notetoheadfaculty/" +
+            control.control.id,
+          {
+            headers: {
+              Authorization: "Bearer " + tok,
+            },
+          }
+        );
+        setNotes(data);
+        // console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getNote();
+  }, []);
 
-    }
-    const isAccepted = true
-    return (
-        <>
-            <div className="container">
-                {/* Title control */}
-                <div className="row text-center">
-                    <div className="col-12">
-                        <div className='Title-Control'>
-                            كنترول الدفعه الثالثه كليه حاسيات و معلومات لعام 2024 تحت ادارة عميد الكليه د/ محمد عبدالرازق و رائس الكنترول د / محمد عبدالرازق
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    getControlSubject();
+    getNote();
+  }, [getControlSubject,getNote]);
 
-                {/* Title Table*/}
-                <div className="row Table-title m-5">
-                    <div className=" text-end">
-                        <div>المقرارت</div>
-                    </div>
+//   const onSendNote = async (event) => {
+//     event.preventDefault();
 
-                </div>
-                {/* Table Material Control */}
-                <div className="container">
-                    <div className="row justify-content-center Table-data">
-                        <div className="col-md-4 text-center ">
-                            <Doughnut
-                                data={data}
-                                options={options}>
-                                
-                            </Doughnut>
-                        </div>
-                        <div className="col-md-8">
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                            {/* Start */}
-                            <div className="col-sm-12 Column-Table d-flex justify-content-end ">
-                                <div className="text-2-column-table">مدخل الي  الشريعه والقانون 1</div>
-                                <div className="state-column-table ">
-                                    {
-                                        isAccepted ? <img src={accepted} alt="accepted" className='ImagIconState' /> : <img src={notaccepted} alt="notaccepted" className='ImagIconState' />
-                                    }
-                                </div>
-                            </div>
-                            {/* end */}
-                        </div>
-                    </div>
-                </div>
+//     const fd = new FormData(event.target);
+//     const formData = Object.fromEntries(fd.entries());
+//     console.log(formData);
+//       const jsonNote = JSON.stringify(formData);
+//     try {
+//       const response = await axios.post(
+//         "http://localhost:5120/controlnotes?Cid=" + control.control.id,
+//         jsonNote,
+//         {
+//           headers: {
+//             Authorization: "Bearer " + tok,
+//             "Content-Type": "application/json",
+//           },
+//         }
+//       );
+//       console.log(response.data);
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+
+  //   const data = {
+  //     labels: ["المواد التي تم انجازها", "المواد التي لو يتم انجازها"],
+  //     datasets: [
+  //       {
+  //         lable: "Poll",
+  //         data: [60, 40],
+  //         backgroundColor: ["rgba(68, 170, 68, 1)", "red"],
+  //         borderColor: ["black", "red"],
+  //       },
+  //     ],
+  //     };
+  //   console.log(controlSubjects);
+  const options = {};
+  const isAccepted = true;
+  return (
+    <>
+      <div className="container">
+        {/* Title control */}
+        <div className="row text-center">
+          <div className="col-12">
+            <div className="Title-Control rtl">
+              كنترول لعام {control.control.acaD_YEAR} تحت ادارة رائس الكنترول د/{" "}
+              {HeadControl}
             </div>
-            {/*  Notes Control */}
-            {/* Send Nots */}
-            <div className="continer">
-                <div className="row justify-content-center m-1 ">
-                    <div className="col-10 border border-info p-3 rounded">
-                        <form >
-                            <textarea placeholder='....ما هي ملاحظاتك' rows="5" cols="65" className='col-12 TextAreaFiled text-end' ></textarea>
-                            <button className='btnSendNotes'>Send</button>
-                        </form>
+          </div>
+        </div>
 
-                    </div>
-                </div>
+        {/* Title Table*/}
+        <div className="row Table-title m-5">
+          <div className="m-0">
+            <div className=" text-end">المقرارت</div>
+          </div>
+          {/* Table Material Control */}
+          <div className="container">
+            <div className="row justify-content-center Table-data m-0">
+              <div className="col-md-4 text-center ">
+                {/* <Doughnut data={data} options={options}></Doughnut> */}
+              </div>
+              <div className="col-md-8 rtl ">
+                {controlSubjects.map((subject) => (
+                  <div key={subject.id} className="subject fs-5">
+                    <span>{subject.name}</span>
+                    <FontAwesomeIcon
+                      className="mx-3 fw-bold"
+                      icon={faCircleCheck}
+                      style={{ color: subject.isDone ? "#44AA44" : "#757575" }}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
-            {/* resever Nots */}
-            <div className="continer">
-                <div className="row justify-content-end m-3 ">
-                    <div className="col-md-12 text-center ">
-                        <div className='Table-title'>ملاحظات رئيس الكنترول</div>
-                    </div>
-                </div>
+          </div>
+        </div>
+      </div>
+      {/*  Notes Control */}
+      {/* Send Nots */}
+      <div className="continer">
+        <div className="row justify-content-center m-1 ">
+          <div className="col-10 border border-info p-3 rounded">
+            <form >
+              <textarea
+                placeholder="....ما هي ملاحظاتك"
+                rows="5"
+                cols="65"
+                className="col-12 TextAreaFiled text-end"
+                name="note"
+              ></textarea>
+              <button className="btnSendNotes">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* resever Nots */}
+      <div className="continer">
+        <div className="row justify-content-end m-3 ">
+          <div className="col-md-12 text-center ">
+            <div className="Table-title">ملاحظات رئيس الكنترول</div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {Notes.map((note) => {
+            return (
+              <div class="col-12 box_Notes  m-3">
+                <div className="boxNotes_Title">{note.description}</div>
                 <div className="row justify-content-center">
-                    {/* Start */}
-                    <div class="col-12 box_Notes  m-3">
-                        <div className='boxNotes_Title'>وجود اوراق ناقصه </div>
-                        <div className="row justify-content-center">
-                            <div className="col-md-6 nameOfMemberNotes">
-                                <div >3/24/2003</div>
-                            </div>
-                            <div className="col-md-6  nameOfMemberNotes">
-                                <div >د/ محمد عبدالرازق</div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* End */}
-                    {/* Start */}
-                    <div class="col-12 box_Notes  m-3">
-                        <div className='boxNotes_Title'>وجود اوراق ناقصه </div>
-                        <div className="row justify-content-center">
-                            <div className="col-md-6 nameOfMemberNotes">
-                                <div >3/24/2003</div>
-                            </div>
-                            <div className="col-md-6  nameOfMemberNotes">
-                                <div >د/ محمد عبدالرازق</div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* End */}
+                  <div className="col-md-6 nameOfMemberNotes">
+                    <div>{note.writeDate}</div>
+                  </div>
+                  <div className="col-md-6  nameOfMemberNotes rtl">
+                    <div>د/ {note.writeBy.name}</div>
+                  </div>
                 </div>
-            </div>
-        </>
-    )
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
-
--- src/Components/Admin Facuilty/Controls/InformationControl.jsx
+++ src/Components/Admin Facuilty/Controls/InformationControl.jsx
@@ -1,77 +1,102 @@
-import React, { useEffect, useState } from 'react'
-import "./styleControls.css"
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
+import React, { useCallback, useEffect, useState } from "react";
+import "./styleControls.css";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { setDetails } from "../../../Redux/detailsSlice";
+
+
 export default function InformationControl() {
+  const [controls, setControls] = useState([]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const tok = useSelector((state) => state.auth.token);
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    const { data } = await axios.get('http://localhost:5120/Controls/allControllers', {
-        headers: {
-            'Authorization': 'Bearer ' + tok
+  const Fid = useSelector((state) => state.Profile.Fid);
+  const semester = `${new Date().getFullYear()}/${
+    new Date().getFullYear() + 1
+  }`;
+  const getControls = useCallback(() => {
+    const getControls = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:5120/Controls/" + Fid,
+          {
+            headers: {
+              Authorization: "Bearer " + tok,
+            },
+          }
+        );
+        for (const control of data) {
+          // console.log(control);
+          try {
+            const {
+              data: { user },
+            } = await axios.get(
+              "http://localhost:5120/users/headConrol/" + control.id,
+              {
+                headers: {
+                  Authorization: "Bearer " + tok,
+                },
+              }
+            );
+            // console.log(user);
+            setControls((prev) => [...prev, { user, control }]);
+          } catch (e) {
+            console.log(e.message);
+          }
         }
-    });
-    setData(data);
-};
-useEffect(() => {
-    getData();
-}, []);
-console.log(data[0]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getControls();
+  }, []);
+
+  useEffect(() => {
+    getControls();
+  }, [getControls]);
+
+  const handleSendControl = (control) => {
+    console.log(control);
+    dispatch(setDetails({ control: control }));
+    navigate("control");
+  };
+
   return (
-    <div className='container HomeClassInfoControls'>
+    <div className="container HomeClassInfoControls">
       {/* Semes_Acad_Title */}
-      <div className='row  Semes_Acad_Title m-2'>
-        <div className="col-5  text-end Semes_Acad_Title-col">2024/2023</div>
+      <div className="row  Semes_Acad_Title m-2">
+        <div className="col-5  text-end Semes_Acad_Title-col">{semester}</div>
         <div className="col-2   Semes_Acad_Title-col">-</div>
-        <div className="col-5   text-start Semes_Acad_Title-col">semester 2</div>
+        <div className="col-5   text-start Semes_Acad_Title-col">
+          semester 2
+        </div>
       </div>
       {/* Semes_Acad_Title */}
-      <div className='container ClassesControls'>
-        <div className='row justify-content-center'>
+      <div className="container ClassesControls">
+        <div className="row justify-content-center">
           {/* Start  */}
-          <div class="col-4 m-3 boxControl ">
-            <div className="row">
-              <div className="col-6 dataofControl">
-                <div className='nameOfHeadControl'>د/ محمد عبدالرازق</div>
-                <div className="nameOfJob">رئيس الكنترول</div>
+          {controls.map((control) => {
+            // console.log(control.control);
+            return (
+              <div
+                className="col-4 m-3 boxControl pointer-event"
+                onClick={() => handleSendControl(control)}
+              >
+                <div className="row">
+                  <div className="col-6 dataofControl">
+                    <div className="nameOfHeadControl">{control.user.name}</div>
+                    <div className="nameOfJob">رئيس الكنترول</div>
+                  </div>
+                  <div className="col-6 text-center nameOfControl">
+                    <div>{control.control.name}</div>
+                  </div>
+                </div>
               </div>
-              <div className="col-6 text-center nameOfControl">
-                <div >المستوي الأول</div>
-              </div>
-            </div>
-          </div>
-          {/* End  */}
-          {/* Start  */}
-          <div class="col-4 m-3 boxControl ">
-            <div className="row">
-              <div className="col-6 dataofControl">
-                <div className='nameOfHeadControl'>د/ محمد عبدالرازق</div>
-                <div className="nameOfJob">رئيس الكنترول</div>
-              </div>
-              <div className="col-6 text-center nameOfControl">
-                <div >المستوي الأول</div>
-              </div>
-            </div>
-          </div>
-          {/* End  */}
-          {/* Start  */}
-          <div class="col-4 m-3 boxControl ">
-            <div className="row">
-              <div className="col-6 dataofControl">
-                <div className='nameOfHeadControl'>د/ محمد عبدالرازق</div>
-                <div className="nameOfJob">رئيس الكنترول</div>
-              </div>
-              <div className="col-6 text-center nameOfControl">
-                <div >المستوي الأول</div>
-              </div>
-            </div>
-          </div>
-          {/* End  */}
+            );
+          })}
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
-
